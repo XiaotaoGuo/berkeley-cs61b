772eba6e949e1cf449a9b9e81f8b740b2ae77108
XiaotaoGuo <45047478+XiaotaoGuo@users.noreply.github.com>
2020-02-17 15:50:33 -0800

    small style fixed

--- proj2/byog/Test/WorldCreatorTest.java
+++ proj2/byog/Test/WorldCreatorTest.java
@@ -6,26 +6,26 @@ public class WorldCreatorTest {
 
 
     public static void main(String[] args) {
-        World m_World = new World(30, 20, 2213);
+        World mWorld = new World(30, 20, 2213);
 
-        m_World.printWorld();
+        mWorld.printWorld();
 
-        m_World.moveCharacter('W');
+        mWorld.moveCharacter('W');
 
-        m_World.printWorld();
+        mWorld.printWorld();
 
-        m_World.moveCharacter('D');
+        mWorld.moveCharacter('D');
 
-        m_World.printWorld();
+        mWorld.printWorld();
 
     }
 
-    public static void runTestRandomGenerate(World m_World) {
+    public static void runTestRandomGenerate(World mWorld) {
 
         int count0 = 0, count1 = 0, count2 = 0, count3 = 0;
         System.out.println("Run random generation between 0 and 3 for 1000 times");
         for (int i = 0; i < 1000; i++) {
-            int choice = m_World.generateRandom(0, 4);
+            int choice = mWorld.generateRandom(0, 4);
             if (choice == 0) {
                 count0++;
             } else if (choice == 1) {
@@ -36,7 +36,8 @@ public class WorldCreatorTest {
                 count3++;
             }
         }
-        System.out.println("0: " + count0 + ", 1: " + count1 + ", 2: " + count2 + ", 3: " + count3 + ".");
+        System.out.println("0: " + count0 + ", 1: "
+                + count1 + ", 2: " + count2 + ", 3: " + count3 + ".");
     }
 
 }
