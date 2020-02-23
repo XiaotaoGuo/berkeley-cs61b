26c6d97cd95b90be80dd2638b53cd9ba0e57e247
XiaotaoGuo <45047478+XiaotaoGuo@users.noreply.github.com>
2020-02-23 00:54:12 -0800

    small fixed

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -8,23 +8,28 @@ import static org.junit.Assert.*;
 public class Percolation {
 
     private WeightedQuickUnionUF sites;
+    private WeightedQuickUnionUF sitesToTop;
     private int top;
     private int bottom;
     private int length;
     private boolean[][] openSites;
     private int openCount;
+    private boolean isPercolate;
 
     public Percolation (int N) {
         if (N <= 0) {
             throw new java.lang.IllegalArgumentException("N should be positive");
         }
+        isPercolate = false;
         length = N;
         top = N * N;
         bottom = N * N + 1;
         sites = new WeightedQuickUnionUF(N * N + 2);
+        sitesToTop = new WeightedQuickUnionUF(N * N + 1);
         openSites = new boolean[N][N];
         openCount = 0;
         for (int i = 0; i < N; i++) {
+            sitesToTop.union(top, posToIndex(0, i));
             for (int j = 0; j < N; j++) {
                 openSites[i][j] = false;
             }
@@ -42,9 +47,6 @@ public class Percolation {
             if (row == 0) {
                 sites.union(top, posToIndex(row, col));
             }
-            if (row == length - 1 && !percolates()) {
-                sites.union(bottom, posToIndex(row, col));
-            }
 
             if (row > 0) {
                 unionHelper(row, col, row - 1, col);
@@ -58,6 +60,13 @@ public class Percolation {
             if (col < length - 1) {
                 unionHelper(row, col, row, col + 1);
             }
+            /* in the last row */
+            if (row == length - 1) {
+                sites.union(bottom, posToIndex(row, col));
+            }
+            if (row == 0) {
+                sites.union(top, posToIndex(row, col));
+            }
         }
     }
 
@@ -66,7 +75,7 @@ public class Percolation {
     }
 
     public boolean isFull(int row, int col) {
-        return openSites[row][col] && sites.connected(top, posToIndex(row, col));
+        return openSites[row][col] && sitesToTop.connected(top, posToIndex(row, col));
     }
 
     public int numberOfOpenSites() {
@@ -94,12 +103,11 @@ public class Percolation {
     private void unionHelper(int row1, int col1, int row2, int col2) {
         if (openSites[row2][col2]) {
             sites.union(posToIndex(row1, col1), posToIndex(row2, col2));
+            sitesToTop.union(posToIndex(row1, col1), posToIndex(row2, col2));
         }
     }
 
     private int posToIndex(int row, int col) {
         return row * length + col;
     }
-
-
 }
