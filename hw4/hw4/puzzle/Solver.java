26013214745557ba543f293d0df247776e475922
XiaotaoGuo <45047478+XiaotaoGuo@users.noreply.github.com>
2020-02-27 16:03:30 -0800

    fix bugs in solver.java

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -1,8 +1,6 @@
 package hw4.puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,40 +9,63 @@ import java.util.Set;
 
 public class Solver {
     private List<WorldState> sol;
+    private Node finish;
 
     public Solver(WorldState initial) {
-        MinPQ<WorldState> pq = new MinPQ<>();
-        pq.insert(initial);
-        Map<WorldState, WorldState> prev = new HashMap<>();
+        MinPQ<Node> pq = new MinPQ<>();
+        Node start = new Node(0, initial, null);
+        pq.insert(start);
         Set<WorldState> visited = new HashSet<>();
-        prev.put(initial, null);
-        WorldState curr = null;
+        Node curr = null;
         while (!pq.isEmpty()) {
-            curr = pq.min();
-            pq.delMin();
-            visited.add(curr);
-            if (curr.isGoal()) {
+            curr = pq.delMin();
+            visited.add(curr.state);
+            if (curr.state.isGoal()) {
+                finish = curr;
                 break;
             }
-            for (WorldState next : curr.neighbors()) {
+            for (WorldState next : curr.state.neighbors()) {
                 if (visited.contains(next)) {
                     continue;
                 }
-                prev.put(next, curr);
-                pq.insert(next);
+                Node n = new Node(curr.moves + 1, next, curr);
+                pq.insert(n);
             }
         }
 
         sol = new ArrayList<>();
         while (curr != null) {
-            sol.add(curr);
-            curr = prev.get(curr);
+            sol.add(curr.state);
+            curr = curr.prev;
         }
 
         Collections.reverse(sol);
 
+    }
+
+    private class Node implements Comparable {
+        private int moves;
+        private int distance;
+        private WorldState state;
+        private Node prev;
+
+        Node(int moves, WorldState state, Node prev) {
+            this.distance = state.estimatedDistanceToGoal();
+            this.moves = moves;
+            this.prev = prev;
+            this.state = state;
+        }
 
 
+        @Override
+        public int compareTo(Object o) {
+            if (o.getClass() == this.getClass()) {
+                Node other = (Node) o;
+                return (this.moves + this.distance) - (other.moves + other.distance);
+            }
+
+            return -1;
+        }
     }
 
     public int moves() {
